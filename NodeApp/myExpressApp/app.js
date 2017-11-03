8ad5ac11800cf2b378d8e21b5216d4018d36e1b9
Austin Overholt <austinoverholt@gmail.com>
2017-11-03 16:34:30 -0700

    Got some more example routes running

--- NodeApp/myExpressApp/app.js
+++ NodeApp/myExpressApp/app.js
@@ -6,13 +6,41 @@ app.get("/", function(req, res){
   res.send("Hello!");
 });
 
+// ":" tells the app it will have a variable after 
+app.get("/r/:name", function(req, res){
+  // will log the route parameter sent in
+  // could do another get with this
+  var subreddit = req.params.name;
+  res.send("Welcome to the " + name + " subreddit!");
+});
+
+app.get("/speak/:animal", function(req, res){
+  var animal = req.params.animal;
+  var sounds = {
+    pig: "Oink",
+    cow: "Moo",
+    dog: "Woof"
+  }
+  var sound = sounds[animal];
+  res.send("The " + animal + " says " + sound);
+});
+
+app.get("/repeat/:string/:num", function(req, res){
+  var string = req.params.string;
+  var result = "";
+  var num = req.params.num;
+  for(var i = 0; i < num; i++){
+    result += " " + string;
+  }
+  res.send(result);
+});
+
 // "*" is a catch all and will return if nothing else is found
 app.get("*", function(req, res){
   res.send("Route not found!");
 });
 
 app.listen(3000);
-
 
 
 
