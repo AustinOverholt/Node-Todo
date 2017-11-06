607c6e2c0640189812a582ee40cdf422b492e455
Austin Overholt <austinoverholt@gmail.com>
2017-11-05 22:08:45 -0800

    Added ejs and changed app

---
+++ NodeApp/ExpressAppDemo/app.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+// ":" tells the app it will have a variable after 
+app.get("/r/:name", function(req, res){
+    // will log the route parameter sent in
+    // could do another get with this
+    var subreddit = req.params.name;
+    res.send("Welcome to the " + name + " subreddit!");
+  });
+  
+  app.get("/speak/:animal", function(req, res){
+    var animal = req.params.animal;
+    var sounds = {
+      pig: "Oink",
+      cow: "Moo",
+      dog: "Woof"
+    }
+    var sound = sounds[animal];
+    res.send("The " + animal + " says " + sound);
+  });
+  
+  app.get("/repeat/:string/:num", function(req, res){
+    var string = req.params.string;
+    var result = "";
+    var num = req.params.num;
+    for(var i = 0; i < num; i++){
+      result += " " + string;
+    }
+    res.send(result);
+  });
+  
+  // "*" is a catch all and will return if nothing else is found
+  app.get("*", function(req, res){
+    res.send("Route not found!");
+  });
+
+app.listen(3000, function(){
+    console.log("Server Started");
+});
